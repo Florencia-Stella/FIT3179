eb9fe29bf3025d396e9d8a4dce3f17852c37b4dc
Florencia-Stella <124228499+Florencia-Stella@users.noreply.github.com>
2023-09-24 12:41:14 +1000

    Delete vega_lite_vis.js

--- vega_lite_vis.js
+++
@@ -1,4 +0,0 @@
-var vg_1 = "https://raw.githubusercontent.com/Florencia-Stella/FIT3179/main/Map.json";
-vegaEmbed("#bar_chart", vg_1).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
